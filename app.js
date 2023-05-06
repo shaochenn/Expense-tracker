9a8169d31a8be0e4f3311e93803b6305aec3c262
shaochenn <hnhngtb1111@gmail.com>
2023-05-07 05:45:27 +0800

    refactor: mongoose config

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-
 const routes = require('./routes')
 
-const Record = require('./models/record')
-const mongoose = require('mongoose')
-
+require('./config/mongoose')
 
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
@@ -18,7 +15,6 @@ const app = express()
 
 const port = process.env.PORT
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
@@ -67,80 +63,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
-// app.get('/', (req, res) => {
-//   let totalAmount = 0
-
-//   Record.find()
-//     .lean()
-//     .then(records => {
-//       records.forEach(record => totalAmount = totalAmount + record.amount)
-//       res.render('index', { records, totalAmount })
-//     })
-//     .catch(error => console.log(error))
-// })
-
-// //create
-// app.get('/records/new', (req, res) => {
-//   res.render('new')
-// })
-
-// app.post('/records', (req, res) => {
-//   const { name, date, categoryId, amount } = req.body
-  
-//   return Record.create({ name, date, categoryId, amount })
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-
-// //update
-// app.get('/records/:id/edit', (req, res) => {
-
-//   const _id = req.params.id
-//   return Record.findOne({ _id })
-//     .lean()
-//     .then(record => res.render('edit', { record }))
-//     .catch(error => console.log(error))
-// })
-
-// app.put('/records/:id', (req, res) => {
-  
-//   const _id = req.params.id
-//   const { name, date, categoryId, amount } = req.body
-
-//   return Record.findOne({ _id })
-//     .then(record => {
-//       record.name = name
-//       record.date = date
-//       record.categoryId = categoryId
-//       record.amount = amount
-//       return record.save()
-//     })
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-// //delete
-// app.delete('/records/:id', (req, res) => {
-
-//   const _id = req.params.id
-//   return Record.findOne({ _id })
-//     .then(record => record.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-
 
 
 app.listen(port, () => {
---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,6 @@
+const Record = require('../record')
+const db = require('../../config/mongoose')
+
+db.once('open', () => {
+  
+})
