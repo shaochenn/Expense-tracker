b0076f96db611ea8373cd8ad6af416f1c21945c7
shaochenn <hnhngtb1111@gmail.com>
2023-05-07 15:01:02 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const usePassport = require('./config/passport')
 const routes = require('./routes')
@@ -70,6 +71,12 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  
+  next()
+})
 app.use(routes)
 
 
